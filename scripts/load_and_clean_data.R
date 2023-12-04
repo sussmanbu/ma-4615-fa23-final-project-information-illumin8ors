a30d3c1308136fcfcb345f007768d592009ffcda
ArianitBalidemaj <aribalid@bu.edu>
2023-12-04 09:47:20 -0500

    Built site for gh-pages

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readr)
+library(readxl)
 
 # Read the CSV data file (make sure to provide the correct arguments)
 load("./dataset/NSDUH_2021.RData")
@@ -48,5 +49,35 @@ drug_health_data_clean_new <- PUF2021_100622 |>
 
 # Save the cleaned data to a new CSV file
 saveRDS(drug_health_data_clean_new, "./dataset/cleaned_data_new.rds")
+
+getwd()
+
+
+Census <- read_excel(path = "./dataset/co-est2019-alldata(1).xlsx", range = "A2:H3194", 
+                   col_names = c("SUMLEV", "REGION", "DIVISION", "STATE", "COUNTY", "STNAME", "CITYNAME","CENSUS2010POP"), 
+                   col_types = c("guess", "text", "text", "guess", "guess", "text", "text", "numeric"))
+
+
+saveRDS(Census, "./dataset/Census_2010_data.rds")
+
+CBSA <- read_excel(path = "./dataset/CBSAdata.xlsx", 
+                   col_names = c("CBSA_Code", "Metro_Division_Code", "CSA_Code", "CBSA Title", 
+                                 "Level_of_CBSA", "Status", "Metropolitan_Division_Title", 
+                                 "CSA_Title", "Component_Name", "State", "FIPS", "County_Status"), 
+                   col_types = c("guess", "guess", "guess", "text", "text", "guess", 
+                                 "text", "text", "text", "text", "guess", "text"), skip = 4, na = c("missing", "NA"))
+
+saveRDS(CBSA, "./dataset/CBSA_data_clean.rds")
+
+#creating a merged data set with location information:
+
+(counties_in_census <- Census |> distinct(CITYNAME))
+
+(counties_in_CBSA <- CBSA |> filter(str_detect(Component_Name, "County")) |> distinct(Component_Name))
+
+county_overlap <- counties_in_CBSA |>
+  filter(counties_in_CBSA %in% counties_in_census) |>
+  pull(counties_in_CBSA)
+
 
 
