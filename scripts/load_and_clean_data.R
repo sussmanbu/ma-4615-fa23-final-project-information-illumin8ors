9dd3114ad88d4df651d94d5bc03f2cadbb349970
Eleanor Paul <eepaul@bu.edu>
2023-12-16 12:53:38 -0500

    fixed

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -125,7 +125,7 @@ less_than_mil__list <- less_than_mil_CBSA |>
 not_in_CBSA <- Clean_census |>
   filter(!CTYNAME %in% county_overlap)
 
-not_CBSA_list < not_in_CBSA |>
+not_CBSA_list <- not_in_CBSA |>
   pull(CTYNAME)
 
 Census_counties <- Clean_census |> filter(STNAME != CTYNAME) |> pull(CTYNAME)
